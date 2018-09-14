de72230517bbbe4339daa7e62aac94be1bb4c212
Katsuhiko Maeno <ka2@ka2.org>
2018-09-14 14:38:49 +0900

    modify dispatcher path of plugin in the tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,7 +23,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/custom-database-tables.php';
+	require dirname( dirname( __FILE__ ) ) . '/cdbt.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
